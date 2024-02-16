da91808c7a51e2fa9db59d391d81a710e84a4abd
Mohamednivazahamed <42490668+Mohamednivazahamed@users.noreply.github.com>
2024-02-16 05:12:16 +0000

    Add files via upload
    day14 commit

---
+++ day14-eruka-registry/src/main/java/com/tcs/eureka/Day14ErukaRegistryApplication.java
@@ -0,0 +1,14 @@
+package com.tcs.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class Day14ErukaRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day14ErukaRegistryApplication.class, args);
+	}
+
+}
